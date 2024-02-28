889f73e16e31f2c75fd196b29c2ca1e7e648a6b6
Kobe Mertens <kobe.mertens@hotmail.com>
2024-02-28 12:05:47 +0100

    update

--- pelicoin_simulator_project_x/hungry_eaters.py
+++ pelicoin_simulator_project_x/hungry_eaters.py
@@ -1,24 +1,16 @@
 import random
+from typing import Callable
 from matplotlib import pyplot as plt
 
 
-MIN_EATERS = 3
-MAX_PARTICIPANTS_AMOUNT = 5
+random.seed(0)
+PARTICIPANTS = ['freyatron', 'imilien', 'juliaan', 'hafthor bjornsson', 'kobalt']
 
-names = ['freyatron', 'imilien', 'juliaan', 'hafthor bjornsson', 'kobalt']
 
-board = { name: 0 for name in names }
+def get_cook_imilien(hungries, board):
+    cook = max(hungries, key=lambda name: board[name])
+    return cook
 
-iterations = 356
-
-cooking_amount = { name: 0 for name in names }
-
-# def get_cook_random(eaters, _):
-#     return eaters[0], eaters[1:]
-
-def get_cook_imilien(participants, board):
-    cook = max(participants, key=lambda name: board[name])
-    return cook, [part for part in participants if part != cook]
 
 def update_board_imilien(cook, eaters, board):
     for eater in eaters:
@@ -26,29 +18,106 @@ def update_board_imilien(cook, eaters, board):
 
     board[cook] = 0
 
-def get_cook_kurb(participants, board):
-    cook = min(participants, key=lambda name: board[name])
-    return cook, [part for part in participants if part != cook]
+
+def get_cook_kurb(hungries, board):
+    cook = min(hungries, key=lambda name: board[name])
+    return cook
+
 
 def update_board_kurb(cook, eaters, board):
     for eater in eaters:
         board[eater] -= 1
+
     board[cook] += len(eaters)
 
-for it in range(iterations):
-    # print(f'============================== ITERATION {it} ===============================')
-    participants_amount = random.randrange(MIN_EATERS, MAX_PARTICIPANTS_AMOUNT + 1)
-    participants = random.sample(names[:MAX_PARTICIPANTS_AMOUNT], participants_amount)
-    # print(f'participants: {participants}')
-    cook, eaters = get_cook_kurb(participants, board)
-    cooking_amount[cook] += len(eaters)
-    # print('cook:', cook)
-    # print('eaters:', eaters)
-    update_board_kurb(cook, eaters, board)
-    # print('board', board)
-    # print()
 
-print(f'cooking amounts: {cooking_amount}')
+def update_meals_balance(cook, hungries, meals_balance):
+    meals_balance['cooked'][cook] += len(hungries)
+    for hungry in hungries:
+        meals_balance['eaten'][hungry] += 1
 
-plt.bar(names, [cooking_amount[name] for name in names])
-plt.show()
+
+class Algorithm:
+    def __init__(self, get_cook, update_board, **config):
+        self.get_cook = get_cook
+        self.update_board = update_board
+        self.config = config
+
+    def run(self, participants):
+        board = { name: 0 for name in participants }
+        meals_balance = { 
+            'cooked': { name: 0 for name in participants }, 
+            'eaten': { name: 0 for name in participants }
+        }
+        for day in range(self.config['cook_days']):
+            hungries_amount = random.randrange(
+                self.config['min_hungries'], 
+                len(participants) + 1
+            )
+            hungries = random.sample(participants, hungries_amount)
+            cook = self.get_cook(hungries, board)
+            update_meals_balance(cook, hungries, meals_balance)
+            eaters = [x for x in hungries if x != cook]
+            self.update_board(cook, eaters, board)
+
+            if 'enable_logging' in self.config and self.config['enable_logging']:
+                log_cooking_day(day, hungries, cook, eaters, board)
+
+        return { 
+            name: meals_balance['cooked'][name] - meals_balance['eaten'][name] 
+            for name in participants
+        }
+
+
+def log_cooking_day(day, hungries, cook, eaters, board):
+    print(f'============================== COOKING DAY {day} ===============================')
+    print(f'hungries: {hungries}')
+    print('cook:', cook)
+    print('eaters:', eaters)
+    print('board', board)
+    print()
+
+
+def main():
+    cook_days = 200
+    min_hungries = 3
+    algorithms = {
+        'kurb': Algorithm(
+            get_cook_kurb, 
+            update_board_kurb, 
+            cook_days=cook_days,
+            min_hungries=min_hungries, 
+        ),
+        'imilien': Algorithm(
+            get_cook_imilien,
+            update_board_imilien,
+            cook_days=cook_days,
+            min_hungries=min_hungries,
+        ),
+        'random': Algorithm(
+            lambda hungries, _: random.choice(hungries),
+            lambda *_: None,
+            cook_days=cook_days,
+            min_hungries=min_hungries,
+        )
+    }
+
+
+    ITERATIONS = 1000
+
+    most_scammed_per_algo = {}
+    for algo_name, algo in algorithms.items():
+        most_scammed = []
+        for it in range(ITERATIONS):
+            diffs = algo.run(PARTICIPANTS)
+            most_scammed.append(max(diffs.values()))
+        
+        most_scammed_per_algo[algo_name] = sum(most_scammed)/len(most_scammed)
+    
+    print(most_scammed_per_algo)
+
+    plt.bar(most_scammed_per_algo.keys(), most_scammed_per_algo.values())
+    plt.show()
+
+if __name__ == '__main__':
+    main()
